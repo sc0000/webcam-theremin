8fc1c572deca8dde3147925ba481791dc65204c8
sc0000 <sebastiancyliax@gmx.net>
2022-07-09 14:08:14 +0200

    Add placeholder button

--- src/Controls.jsx
+++ src/Controls.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Controls = () => {
+  const [startButton, setStartButton] = useState('Start Audio');
+
   return (
     <section id="controls">
-        Controls
+        <h3>Controls</h3>
+        <div>
+          <div className="btn" onClick={() => setStartButton(startButton == 'Stop Audio' ? 'Start Audio' : 'Stop Audio')}>{startButton}</div>
+          {/* <div className="btn">{startButton}</div>
+          <div className="btn">{startButton}</div>
+          <div className="btn">{startButton}</div> */}
+        </div>
+        
     </section>
   )
 }
